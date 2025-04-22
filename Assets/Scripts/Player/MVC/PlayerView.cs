9feba24cd88726a16e85b921a8acc2601ada4fb7
Deependra-Das <iamdeep75@gmail.com>
2025-04-23 04:13:12 +0530

    Changes for Player Animations for Movement & Jump

--- Assets/Scripts/Player/MVC/PlayerView.cs
+++ Assets/Scripts/Player/MVC/PlayerView.cs
@@ -1,3 +1,4 @@
+using Unity.Android.Gradle.Manifest;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,6 +10,7 @@ namespace BattleRoyale.Player
     {
         [SerializeField] private GameObject _cinemachineCameraTarget;
         [SerializeField] private CharacterController _controller;
+        [SerializeField] private Animator _animator;
         [SerializeField] private PlayerInput _playerInput;
 
         [Header("Cinemachine")]
@@ -39,24 +41,57 @@ namespace BattleRoyale.Player
             }
         }
 
-        public float MoveSpeed = 5.0f;
+        public float MoveSpeed = 10.0f;
         [Range(0.0f, 0.3f)]
         public float RotationSmoothTime = 0.12f;
-        public float SpeedChangeRate = 10.0f;
-        private float _speed;
-        private float _animationBlend;
+        public float SpeedChangeRate = 20.0f;
+        public float VerticalSpeedChangeRate = 0.5f;
+        public float FallGravityClamp = -20.0f;
+        public float MaxJumpTime = 1.5f;
+        public float gravity = -9.8f;
+        public float GroundedGravity = -0.05f;
+        public float FallMultiplier = 2.0f;
+
         private float _targetRotation = 0.0f;
-        private float _rotationVelocity;
-        private float _verticalVelocity;
+        private float _rotationVelocity = 0.0f;
+        private float _appliedverticalVelocity = 0.0f;
+        private float _currentVerticalVelocity;
+        private float _initialJumpVelocity;
+
+
+        private bool _isJumping = false;
+        private bool _isJumpingAnimating = false;
+        private float _animationBlend;
+        private int _animIDSpeed;
+        private int _animIDJump;
 
         private void Start()
         {
             _cinemachineTargetYaw = _cinemachineCameraTarget.transform.rotation.eulerAngles.y;
+
+            AssignAnimationIDs();
+            SetupJumpVariables();
         }
+
+        private void AssignAnimationIDs()
+        {
+            _animIDSpeed = Animator.StringToHash("Speed");
+            _animIDJump = Animator.StringToHash("Jump");
+        }
+
+        private void SetupJumpVariables()
+        {
+            float timeToApex = MaxJumpTime / 2;
+            gravity = (-2 * MaxJumpTime) / Mathf.Pow(timeToApex, 2);
+            _initialJumpVelocity = (2 * MaxJumpTime) / timeToApex;
+        }
+
 
         private void Update()
         {
             HandleMovement();
+            HandleGravity();
+            HandleJump();
         }
 
         private void LateUpdate()
@@ -92,10 +127,57 @@ namespace BattleRoyale.Player
             Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
             _controller.Move(targetDirection.normalized * (targetSpeed * Time.deltaTime) +
-                             new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+                             new Vector3(0.0f, _appliedverticalVelocity, 0.0f) * Time.deltaTime);
+
+            _animator.SetFloat(_animIDSpeed, _animationBlend);
 
         }
 
+        private void HandleGravity()
+        {
+            bool isFalling = _currentVerticalVelocity <= 0.0f || !jump;
+
+            if (_controller.isGrounded)
+            {
+                if (_isJumpingAnimating)
+                {
+                    _animator.SetBool(_animIDJump, false);
+                    _isJumpingAnimating = false;
+                }
+
+                _currentVerticalVelocity = GroundedGravity;
+                _appliedverticalVelocity = GroundedGravity;
+            }
+            else if (isFalling)
+            {
+                float prevVelocity_Y = _currentVerticalVelocity;
+                _currentVerticalVelocity = _currentVerticalVelocity + (gravity * FallMultiplier * Time.deltaTime);
+                _appliedverticalVelocity = Mathf.Max((prevVelocity_Y + _currentVerticalVelocity) * VerticalSpeedChangeRate, FallGravityClamp);
+            }
+            else
+            {
+                float prevVelocity_Y = _currentVerticalVelocity;
+                _currentVerticalVelocity = _currentVerticalVelocity + (gravity * Time.deltaTime);
+                _appliedverticalVelocity = (prevVelocity_Y + _currentVerticalVelocity) * VerticalSpeedChangeRate;
+            }
+        }
+
+        private void HandleJump()
+        {
+            if (!_isJumping && _controller.isGrounded && jump)
+            {
+                _animator.SetBool(_animIDJump, true);
+                _isJumpingAnimating = true;
+                _isJumping = true;
+                jump = false;
+                _currentVerticalVelocity = _initialJumpVelocity;
+                _appliedverticalVelocity = _initialJumpVelocity;
+            }
+            else if (_isJumping && _controller.isGrounded && !jump)
+            {
+                _isJumping = false;
+            }
+        }
 
         private void CameraRotation()
         {
