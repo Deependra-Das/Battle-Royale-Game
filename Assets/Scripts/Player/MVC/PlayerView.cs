be27ce297835fca11e5fd6c774417354ce969d98
Deependra-Das <iamdeep75@gmail.com>
2025-04-23 01:28:35 +0530

    Changes to move Character

--- Assets/Scripts/Player/MVC/PlayerView.cs
+++ Assets/Scripts/Player/MVC/PlayerView.cs
@@ -1,13 +1,14 @@
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 namespace BattleRoyale.Player
 {
     public class PlayerView : MonoBehaviour
     {
         [SerializeField] private GameObject _cinemachineCameraTarget;
-
+        [SerializeField] private CharacterController _controller;
         [SerializeField] private PlayerInput _playerInput;
 
         [Header("Cinemachine")]
@@ -38,15 +39,63 @@ namespace BattleRoyale.Player
             }
         }
 
+        public float MoveSpeed = 5.0f;
+        [Range(0.0f, 0.3f)]
+        public float RotationSmoothTime = 0.12f;
+        public float SpeedChangeRate = 10.0f;
+        private float _speed;
+        private float _animationBlend;
+        private float _targetRotation = 0.0f;
+        private float _rotationVelocity;
+        private float _verticalVelocity;
+
         private void Start()
         {
             _cinemachineTargetYaw = _cinemachineCameraTarget.transform.rotation.eulerAngles.y;
+        }
+
+        private void Update()
+        {
+            HandleMovement();
         }
 
         private void LateUpdate()
         {
             CameraRotation();
         }
+
+        private void HandleMovement()
+        {
+            float targetSpeed = MoveSpeed;
+
+            if (move == Vector2.zero) targetSpeed = 0.0f;
+
+            float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+
+            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+            if (_animationBlend < 0.01f) _animationBlend = 0f;
+
+            Vector3 inputDirection = new Vector3(move.x, 0.0f, move.y).normalized;
+
+            if (move != Vector2.zero)
+            {
+                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
+                                  Camera.main.gameObject.transform.eulerAngles.y;
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
+                    RotationSmoothTime);
+
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            }
+
+
+            Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+            _controller.Move(targetDirection.normalized * (targetSpeed * Time.deltaTime) +
+                             new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+        }
+
 
         private void CameraRotation()
         {
