21cb9b4e70e886cee5044202d70c247681be809d
Deependra-Das <iamdeep75@gmail.com>
2025-04-23 16:57:18 +0530

    Refactoring Animation & Added SO, Service, Controller & Model

--- Assets/Scripts/Player/MVC/PlayerView.cs
+++ Assets/Scripts/Player/MVC/PlayerView.cs
@@ -1,5 +1,4 @@
 using Unity.Android.Gradle.Manifest;
-using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Windows;
@@ -9,22 +8,43 @@ namespace BattleRoyale.Player
     public class PlayerView : MonoBehaviour
     {
         [SerializeField] private GameObject _cinemachineCameraTarget;
-        [SerializeField] private CharacterController _controller;
+        [SerializeField] private CharacterController _charController;
         [SerializeField] private Animator _animator;
         [SerializeField] private PlayerInput _playerInput;
+
+      
+        private PlayerModel _playerModel;
+        private bool _isPlayerInitialized = false;
+
+        private bool Grounded = true;
+        public LayerMask GroundLayers;
 
         [Header("Cinemachine")]
         public float TopClamp = 70.0f;
         public float BottomClamp = -30.0f;
         public float CameraAngleOverride = 0.0f;
+
         public bool LockCameraPosition = false;
 
         private float _cinemachineTargetYaw;
         private float _cinemachineTargetPitch;
 
+        private float _animationBlend;
+        private float _targetRotation = 0.0f;
+        private float _rotationVelocity;
+        private float _verticalVelocity;
+
+        private float _jumpTimeoutDelta;
+        private float _fallTimeoutDelta;
+
+        private int _animIDSpeed;
+        private int _animIDGrounded;
+        private int _animIDJump;
+        private int _animIDFreeFall;
 
         private const float _threshold = 0.01f;
 
+        [Header("Player Input")]
         public Vector2 move;
         public Vector2 look;
         public bool jump;
@@ -32,85 +52,51 @@ namespace BattleRoyale.Player
         public bool analogMovement;
         public bool cursorLocked = true;
         public bool cursorInputForLook = true;
+        private bool IsCurrentDeviceMouse { get { return _playerInput.currentControlScheme == "KeyboardMouse"; } }
 
-        private bool IsCurrentDeviceMouse
+
+        public void Initialize(PlayerModel playerModel)
         {
-            get
-            {
-                return _playerInput.currentControlScheme == "KeyboardMouse";
-            }
-        }
-
-        public float MoveSpeed = 10.0f;
-        [Range(0.0f, 0.3f)]
-        public float RotationSmoothTime = 0.12f;
-        public float SpeedChangeRate = 20.0f;
-        public float VerticalSpeedChangeRate = 0.5f;
-        public float FallGravityClamp = -20.0f;
-        public float MaxJumpTime = 1.5f;
-        public float gravity = -9.8f;
-        public float GroundedGravity = -0.05f;
-        public float FallMultiplier = 2.0f;
-
-        private float _targetRotation = 0.0f;
-        private float _rotationVelocity = 0.0f;
-        private float _appliedverticalVelocity = 0.0f;
-        private float _currentVerticalVelocity;
-        private float _initialJumpVelocity;
-
-
-        private bool _isJumping = false;
-        private bool _isJumpingAnimating = false;
-        private float _animationBlend;
-        private int _animIDSpeed;
-        private int _animIDJump;
-
-        private void Start()
-        {
+            _playerModel = playerModel;
+            _isPlayerInitialized = true;
             _cinemachineTargetYaw = _cinemachineCameraTarget.transform.rotation.eulerAngles.y;
-
             AssignAnimationIDs();
-            SetupJumpVariables();
-        }
 
-        private void AssignAnimationIDs()
-        {
-            _animIDSpeed = Animator.StringToHash("Speed");
-            _animIDJump = Animator.StringToHash("Jump");
-        }
-
-        private void SetupJumpVariables()
-        {
-            float timeToApex = MaxJumpTime / 2;
-            gravity = (-2 * MaxJumpTime) / Mathf.Pow(timeToApex, 2);
-            _initialJumpVelocity = (2 * MaxJumpTime) / timeToApex;
+            _jumpTimeoutDelta = _playerModel.JumpTimeout;
+            _fallTimeoutDelta = _playerModel.FallTimeout;
         }
 
 
         private void Update()
         {
-            HandleMovement();
-            HandleGravity();
-            HandleJump();
+            if (_isPlayerInitialized)
+            {
+                HandleJumpAndGravity();
+                GroundedCheck();
+                HandleMovement();
+            }
         }
 
         private void LateUpdate()
         {
-            CameraRotation();
+            HandleCameraRotation();
+        }
+
+        private void AssignAnimationIDs()
+        {
+            _animIDSpeed = Animator.StringToHash("Speed");
+            _animIDGrounded = Animator.StringToHash("Grounded");
+            _animIDJump = Animator.StringToHash("Jump");
+            _animIDFreeFall = Animator.StringToHash("FreeFall");
         }
 
         private void HandleMovement()
         {
-            float targetSpeed = MoveSpeed;
-
+            float targetSpeed = _playerModel.MoveSpeed;
             if (move == Vector2.zero) targetSpeed = 0.0f;
 
-            float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
-
-
-            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * _playerModel.SpeedChangeRate);
             if (_animationBlend < 0.01f) _animationBlend = 0f;
-
             Vector3 inputDirection = new Vector3(move.x, 0.0f, move.y).normalized;
 
             if (move != Vector2.zero)
@@ -118,7 +104,7 @@ namespace BattleRoyale.Player
                 _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
                                   Camera.main.gameObject.transform.eulerAngles.y;
                 float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
-                    RotationSmoothTime);
+                    _playerModel.RotationSmoothTime);
 
                 transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
             }
@@ -126,72 +112,79 @@ namespace BattleRoyale.Player
 
             Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
-            _controller.Move(targetDirection.normalized * (targetSpeed * Time.deltaTime) +
-                             new Vector3(0.0f, _appliedverticalVelocity, 0.0f) * Time.deltaTime);
+            _charController.Move(targetDirection.normalized * (targetSpeed * Time.deltaTime) +
+                             new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
 
             _animator.SetFloat(_animIDSpeed, _animationBlend);
-
         }
 
-        private void HandleGravity()
+        private void HandleJumpAndGravity()
         {
-            bool isFalling = _currentVerticalVelocity <= 0.0f || !jump;
-
-            if (_controller.isGrounded)
+            if (Grounded)
             {
-                if (_isJumpingAnimating)
+                _fallTimeoutDelta = _playerModel.FallTimeout;
+                _animator.SetBool(_animIDJump, false);
+                _animator.SetBool(_animIDFreeFall, false);
+
+                if (_verticalVelocity < 0.0f)
                 {
-                    _animator.SetBool(_animIDJump, false);
-                    _isJumpingAnimating = false;
+                    _verticalVelocity = -2f;
                 }
-
-                _currentVerticalVelocity = GroundedGravity;
-                _appliedverticalVelocity = GroundedGravity;
-            }
-            else if (isFalling)
-            {
-                float prevVelocity_Y = _currentVerticalVelocity;
-                _currentVerticalVelocity = _currentVerticalVelocity + (gravity * FallMultiplier * Time.deltaTime);
-                _appliedverticalVelocity = Mathf.Max((prevVelocity_Y + _currentVerticalVelocity) * VerticalSpeedChangeRate, FallGravityClamp);
+                if (jump && _jumpTimeoutDelta <= 0.0f)
+                {
+                    _verticalVelocity = Mathf.Sqrt(_playerModel.JumpHeight * -2f * _playerModel.Gravity);
+                    _animator.SetBool(_animIDJump, true);
+                }
+                if (_jumpTimeoutDelta >= 0.0f)
+                {
+                    _jumpTimeoutDelta -= Time.deltaTime;
+                }
             }
             else
             {
-                float prevVelocity_Y = _currentVerticalVelocity;
-                _currentVerticalVelocity = _currentVerticalVelocity + (gravity * Time.deltaTime);
-                _appliedverticalVelocity = (prevVelocity_Y + _currentVerticalVelocity) * VerticalSpeedChangeRate;
-            }
-        }
+                _jumpTimeoutDelta = _playerModel.JumpTimeout;
 
-        private void HandleJump()
-        {
-            if (!_isJumping && _controller.isGrounded && jump)
-            {
-                _animator.SetBool(_animIDJump, true);
-                _isJumpingAnimating = true;
-                _isJumping = true;
+                if (_fallTimeoutDelta >= 0.0f)
+                {
+                    _fallTimeoutDelta -= Time.deltaTime;
+                }
+                else
+                {
+                    _animator.SetBool(_animIDFreeFall, true);
+                }
+
                 jump = false;
-                _currentVerticalVelocity = _initialJumpVelocity;
-                _appliedverticalVelocity = _initialJumpVelocity;
             }
-            else if (_isJumping && _controller.isGrounded && !jump)
+
+            if (_verticalVelocity < _playerModel.TerminalVelocity)
             {
-                _isJumping = false;
+                _verticalVelocity += _playerModel.Gravity * Time.deltaTime;
             }
         }
+        private void GroundedCheck()
+        {
+            Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - _playerModel.GroundedOffset,
+                transform.position.z);
+            Grounded = Physics.CheckSphere(spherePosition, _playerModel.GroundedRadius, GroundLayers,
+                QueryTriggerInteraction.Ignore);
 
-        private void CameraRotation()
+            _animator.SetBool(_animIDGrounded, Grounded);
+        }
+
+        private void HandleCameraRotation()
         {
             if (look.sqrMagnitude >= _threshold && !LockCameraPosition)
             {
                 float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+
                 _cinemachineTargetYaw += look.x * deltaTimeMultiplier;
                 _cinemachineTargetPitch += look.y * deltaTimeMultiplier;
             }
-
             _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
             _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
 
-            _cinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
+            _cinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+                _cinemachineTargetYaw, 0.0f);
         }
 
         private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
@@ -199,6 +192,18 @@ namespace BattleRoyale.Player
             if (lfAngle < -360f) lfAngle += 360f;
             if (lfAngle > 360f) lfAngle -= 360f;
             return Mathf.Clamp(lfAngle, lfMin, lfMax);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+            Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+
+            if (Grounded) Gizmos.color = transparentGreen;
+            else Gizmos.color = transparentRed;
+            Gizmos.DrawSphere(
+                new Vector3(transform.position.x, transform.position.y - _playerModel.GroundedOffset, transform.position.z),
+                _playerModel.GroundedRadius);
         }
 
         public void OnMove(InputValue value)
@@ -228,5 +233,8 @@ namespace BattleRoyale.Player
         {
             Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
         }
+
+
+        public GameObject PlayerCameraRoot { get { return _cinemachineCameraTarget; } }
     }
 }
