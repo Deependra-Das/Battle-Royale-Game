ad33d86883e5ae5fa6c3b42bbae6eccc228d1a84
Deependra-Das <iamdeep75@gmail.com>
2025-09-22 16:37:29 +0530

    Changes to switch materials for the character skin

--- Assets/Scripts/Network/CharacterSelectPlayer.cs
+++ Assets/Scripts/Network/CharacterSelectPlayer.cs
@@ -2,9 +2,9 @@ using BattleRoyale.Event;
 using BattleRoyale.Network;
 using System;
 using TMPro;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
-using Unity.Collections;
 
 namespace BattleRoyale.CharacterSelection
 {
@@ -13,9 +13,12 @@ namespace BattleRoyale.CharacterSelection
         [SerializeField] private TMP_Text _usernameText;
         [SerializeField] private GameObject _readyStatusLabel;
         [SerializeField] private GameObject _notReadyStatusLabel;
+        [SerializeField] private Material[] _skinMaterials;
+        [SerializeField] private SkinnedMeshRenderer[] _skinnedMeshRenderersForBodyParts;
 
         private NetworkVariable<FixedString128Bytes> _usernameNetworkText = new NetworkVariable<FixedString128Bytes>("Player");
         private NetworkVariable<bool> _readyStatusNetworkBool = new NetworkVariable<bool>(false);
+        private NetworkVariable<int> _selectedMaterialIndex = new NetworkVariable<int>(0);
 
         private int _playerIndex = -1;
 
@@ -26,12 +29,14 @@ namespace BattleRoyale.CharacterSelection
         {
             _usernameNetworkText.OnValueChanged += UpdateCharacterUsername;
             _readyStatusNetworkBool.OnValueChanged += UpdateCharacterLobbyStatus;
+            _selectedMaterialIndex.OnValueChanged += ApplySelectedMaterial;
         }
 
         private void UnsubscribeToEvents()
         {
             _usernameNetworkText.OnValueChanged -= UpdateCharacterUsername;
             _readyStatusNetworkBool.OnValueChanged -= UpdateCharacterLobbyStatus;
+            _selectedMaterialIndex.OnValueChanged -= ApplySelectedMaterial;
         }
 
         public override void OnNetworkSpawn()
@@ -47,6 +52,7 @@ namespace BattleRoyale.CharacterSelection
             {
                 SetPlayerIndexForCharacter(assignedClientIndex);
                 SetUsernameServerRpc(usernameText);
+                SetCharacterSkinMaterial(3);
             }
         }
 
@@ -83,6 +89,32 @@ namespace BattleRoyale.CharacterSelection
             _notReadyStatusLabel.SetActive(!_readyStatusNetworkBool.Value);
         }
 
+        public void SetCharacterSkinMaterial(int materialIndex)
+        {
+            SetMaterialIndexServerRpc(materialIndex);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void SetMaterialIndexServerRpc(int materialIndex)
+        {
+            if (materialIndex >= 0 && materialIndex < _skinMaterials.Length)
+            {
+                _selectedMaterialIndex.Value = materialIndex;
+            }
+        }
+
+        private void ApplySelectedMaterial(int oldMaterialIndex, int newMaterialIndex)
+        {
+            if (_skinnedMeshRenderersForBodyParts != null && newMaterialIndex >= 0 && newMaterialIndex < _skinMaterials.Length)
+            {
+                foreach (SkinnedMeshRenderer renderer in _skinnedMeshRenderersForBodyParts)
+                {
+                    Material[] materialsToRemap = renderer.materials;
+                    materialsToRemap[0] = _skinMaterials[newMaterialIndex];
+                    renderer.materials = materialsToRemap;
+                }
+            }
+        }
 
         private void ShowCharacter() => this.gameObject.SetActive(true);
         private void HideCharacter() => this.gameObject.SetActive(false);
