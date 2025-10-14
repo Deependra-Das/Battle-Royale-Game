b21ea2002e283c1a09536ad35564121d1ddcaaab
Deependra-Das <iamdeep75@gmail.com>
2025-10-14 16:55:19 +0530

    Changes to add Audio Manager

---
+++ Assets/Scripts/Audio/AudioType.cs
@@ -0,0 +1,26 @@
+namespace BattleRoyale.AudioModule
+{
+    public enum AudioType
+    {
+        MainMenuBGM,
+        LobbyBGM,
+        CharSelectionBGM,
+        GameplayBGM,
+        GameOverBGM,
+        ButtonClick,
+        SuccessPopUp,
+        ErrorPopUp,
+        ConfirmationPopUp,
+        DisconnectionPopUp,
+        EliminationdPopUp,
+        GameEndedPopUp,
+        PlayerJoined,
+        PlayerLeft,
+        CharacterSkinChanged,
+        GameStartCountdown,
+        GameStarted,   
+        PlayerFootstep,
+        PlayerJumpLand,
+    }
+}
+
